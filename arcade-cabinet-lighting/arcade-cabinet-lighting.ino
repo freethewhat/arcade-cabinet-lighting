7e47ed860957accdb005c090b6cd759f162d67e8
Matt Sutton <mcsutton@outlook.com>
2020-08-04 22:13:33 -0700

    add comments

--- arcade-cabinet-lighting/arcade-cabinet-lighting.ino
+++ arcade-cabinet-lighting/arcade-cabinet-lighting.ino
@@ -13,20 +13,24 @@ ButtonClass button;
 PirSensorClass sensor;
 LedStripClass led;
 
+// Set the default system status.
 boolean systemOn = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(9600);
-	Serial.println("Start");
-
+	// setup the button with the pin the button is using.
 	button.setup(D2);
+
+	// setup the PIR sensor with the pin the sensor is using.
 	sensor.setup(D6);
+
+	// setup the LED with the pin the sensor is using.
 	led.setup(D5);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+	// check to see if button is pressed and toggle the led fade status.
 	if (button.isPressed()) {
 		led.toggleFade();
 	}
@@ -34,11 +38,18 @@ void loop() {
 	sensor.update();
 
 	if (sensor.motionDetected() && !systemOn) {
+		// power on the system and turn the lights on if there system is off
+		// and the sensor detects motion.
 		systemOn = !systemOn;
 		led.on();
 	}
 	else if (!sensor.motionDetected() && systemOn)
 	{
+		// post off the system and turn the light off is the system is on
+		// and detects no motion. 
+	
+		// The sensor changes the motion detected 
+		// state using a timer. So the turning off the system will be delayed.
 		systemOn = !systemOn;
 		led.off();
 	}
