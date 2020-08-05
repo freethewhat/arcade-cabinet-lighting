0f600d68ffbfe77df16bf41f5f60982b0102561e
Matt Sutton <mcsutton@outlook.com>
2020-08-04 20:16:57 -0700

    Add system status

--- arcade-cabinet-lighting/arcade-cabinet-lighting.ino
+++ arcade-cabinet-lighting/arcade-cabinet-lighting.ino
@@ -9,7 +9,9 @@
 #include "Button.h"
 
 ButtonClass button;
-PirSensorClass sensor(D6);
+PirSensorClass sensor;
+
+boolean systemOn = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -17,6 +19,7 @@ void setup() {
 	Serial.println("Start");
 
 	button.setup(D2);
+	sensor.setup(D6);
 }
 
 // the loop function runs over and over again until power down or reset
@@ -28,9 +31,14 @@ void loop() {
 	
 	sensor.update();
 
-	//if (sensor.motionDetected())
-	//	Serial.println("Motion Detected");
-	//else if (!sensor.motionDetected())
-	//	Serial.println("Motion Not Detected");
+	if (sensor.motionDetected() && !systemOn) {
+		Serial.println("System On");
+		systemOn = !systemOn;
+	}
+	else if (!sensor.motionDetected() && systemOn)
+	{
+		Serial.println("System Off");
+		systemOn = !systemOn;
+	}
 }
 
